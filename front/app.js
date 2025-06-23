f357b9dbc0b2944b07121e2b96726da31a26cbba
javi340 <javiernewton870@gmail.com>
2025-06-23 19:40:03 -0300

    test commit

--- front/app.js
+++ front/app.js
@@ -6,7 +6,6 @@ const headers = {
     "Content-Type": "application/json",
     "Authorization": `Bearer ${API_KEY}`
 };
-//jjjj
 // Funciones de servicio que retornan Promesas
 async function registerStudentService(name, career) {
     const response = await fetch(API_URL, {
